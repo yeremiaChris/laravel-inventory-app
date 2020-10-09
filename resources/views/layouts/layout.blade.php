8e145ed5d4849ae60658285a6554e161b5f83c07
yeremiaChris <yeremia997@gmail.com>
2020-10-09 15:34:02 +0700

    menyelesaikan authentication

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
         <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
@@ -12,7 +12,7 @@
             crossorigin="anonymous"
         />
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     </head>
     <body class="antialiased">
@@ -22,7 +22,42 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <!-- Authentication Links -->
+      @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+        
+    </ul>
+  </div>
         </div>
+        
     </nav>
 
     <div class="container mt-3">
@@ -33,24 +68,32 @@
         @endif
     </div>
 
+
     <div class="container">
-        <div class="row ">
-            <div class="col-md-2">
-                    <nav >
-                        <div class="col-sm-12" id="fixed-sidebar">
-                            <!-- <a href="index.html"><img id="home-logo" src="../media/logo-prueba.jpg" alt="Logo de Animanoir"></a>  -->
-                            <ul>
-                                <li class="fuente-fjalla ul-personalizada"><img src="/img/dashboard.png" alt=""><a href="/">Dahsboard</a></li>
-                                <li class="fuente-fjalla ul-personalizada"><img src="/img/item.png" alt=""> <a href="{{ route('jual.create') }}">Form Jual</a></li>
-                                <li class="fuente-fjalla ul-personalizada"><img src="/img/item.png" alt=""> <a href="{{ route('jual.index') }}">Rekapan Penjualan</a></li>
-                                <li class="fuente-fjalla ul-personalizada"><img src="/img/supplier.png" alt=""> <a href="{{ route('supplier.index') }}">Daftar Supplier</a> </li>
-                                <li class="fuente-fjalla ul-personalizada">Blog</li>
-                                <li class="fuente-fjalla ul-personalizada"><a href="acerca.html">Acerca</a></li>
-                                <li class="fuente-fjalla ul-personalizada">Contacto</li>
-                            </ul>
-                        </div>
-                    </nav>
-            </div>
+    @guest
+            @if (Route::has('register'))
+                <div></div>
+            @endif
+        @else
+                <div class="row ">
+                    <div class="col-md-2">
+                            <nav >
+                                <div class="col-sm-12" id="fixed-sidebar">
+                                    <!-- <a href="index.html"><img id="home-logo" src="../media/logo-prueba.jpg" alt="Logo de Animanoir"></a>  -->
+                                    <ul>
+                                        <li class="fuente-fjalla ul-personalizada"><img src="/img/dashboard.png" alt=""><a href="/">Dahsboard</a></li>
+                                        <li class="fuente-fjalla ul-personalizada"><img src="/img/item.png" alt=""> <a href="{{ route('jual.create') }}">Form Jual</a></li>
+                                        <li class="fuente-fjalla ul-personalizada"><img src="/img/item.png" alt=""> <a href="{{ route('jual.index') }}">Rekapan Penjualan</a></li>
+                                        <li class="fuente-fjalla ul-personalizada"><img src="/img/supplier.png" alt=""> <a href="{{ route('supplier.index') }}">Daftar Supplier</a> </li>
+                                        <li class="fuente-fjalla ul-personalizada">Blog</li>
+                                        <li class="fuente-fjalla ul-personalizada"><a href="acerca.html">Acerca</a></li>
+                                        <li class="fuente-fjalla ul-personalizada">Contacto</li>
+                                    </ul>
+                                </div>
+                            </nav>
+                    </div>
+
+        @endguest
             <div class="col-md-10">
                 <div class="container">
                     @yield('content')
@@ -59,7 +102,7 @@
         </div>
     </div>
 
-        <script src="/js/script.js"></script>
+        <script src="/js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
         <script
             src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
